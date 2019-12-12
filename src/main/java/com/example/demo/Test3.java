b8967aba934d255dd44012f9009a8b2ff89e9ed5
Fa <183461750@qq.com>
2019-12-13 01:25:51 +0800

    第一次提交到dev

---
+++ src/main/java/com/example/demo/Test3.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("demo提交到dev");
+    }
+
+}
